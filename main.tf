14e8e09da4ce54ce45c4020402a955a461ea5851
Lazar Linux <vladyslav.lazar.ri.2022@lpnu.ua>
2024-03-20 02:48:04 +0200

    initial commit (added basic dynamodb functions)

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "table_authors" {
+    source = "./modules/dynamodb"
+    table_name = "authors"
+}
+
+module "table_courses" {
+    source = "./modules/dynamodb"
+    table_name = "courses"
+}
