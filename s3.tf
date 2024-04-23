071c9b303bee663f581b748da05eb9dbb0e5471b
Lazar Linux <vladyslav.lazar.ri.2022@lpnu.ua>
2024-04-23 19:23:56 +0300

    CloudFront implementation.

--- s3.tf
+++ s3.tf
@@ -59,3 +59,39 @@ resource "aws_s3_object" "this" {
   content = each.value.content
   etag = each.value.digests.md5
 }
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id = aws_s3_bucket.this.id
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.this.id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  tags = {
+    Environment = "dev"
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
